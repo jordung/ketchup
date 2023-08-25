368a426dfb984184704c8e051458a4ce478164ba
jordung <jordanayd@gmail.com>
2023-08-25 15:46:59 +0800

    finished setup, navbar, landing, spinner, login, signup

---
+++ frontend/src/components/Spinner.js
@@ -0,0 +1,12 @@
+import loading from "../assets/spinner/bottle-filling.gif";
+
+function Spinner() {
+  return (
+    <div className="absolute h-screen w-screen backdrop-blur-md flex flex-col justify-center items-center">
+      <img className="h-40" src={loading} alt="spinning" />
+      <p className="text-neutral font-semibold">Loading...</p>
+    </div>
+  );
+}
+
+export default Spinner;
