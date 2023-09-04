fb383ce38788ac81024180fe1d3c548cd07c1f70
jordung <jordanayd@gmail.com>
2023-09-05 00:42:10 +0800

    updated socket

--- frontend/src/utils/socket-client.js
+++ frontend/src/utils/socket-client.js
@@ -1,3 +1,13 @@
 import { io } from "socket.io-client";
 
-export const socket = io(process.env.REACT_APP_DB_API);
+// export const socket = io(process.env.REACT_APP_DB_API);
+export const socket = io("https://ketchup.fly.dev", {
+  withCredentials: true,
+  transportOptions: {
+    polling: {
+      extraHeaders: {
+        "Access-Control-Allow-Origin": "https://theketchupcorner.netlify.app",
+      },
+    },
+  },
+});
