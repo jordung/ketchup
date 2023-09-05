d7508563e92f3f906613e92871670cf424fd9575
jordung <jordanayd@gmail.com>
2023-09-05 23:05:16 +0800

    slack bugfix

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -65,21 +65,27 @@ function Profile() {
     }
   }, [searchQuery, watchlist]);
 
-  const handleSlackAuth = () => {
-    // Redirect to your server's Slack OAuth route
-    window.location.href = `${process.env.REACT_APP_DB_API}/users/slack/oauth`;
-  };
+  // const handleSlackAuth = () => {
+  //   // Redirect to your server's Slack OAuth route
+  //   window.location.href = `${process.env.REACT_APP_DB_API}/users/slack/oauth`;
+  // };
 
   const renderSlackStatus = (userProfileId, userId, slackUserId) => {
     // case 1: own profile + no slack -> return connect button
     if (userProfileId === userId && !slackUserId) {
       return (
-        <button
+        // <button
+        //   className="btn btn-neutral btn-sm normal-case text-sm"
+        //   onClick={handleSlackAuth}
+        // >
+        //   Connect Slack
+        // </button>
+        <a
+          href="https://slack.com/oauth/v2/authorize?scope=team%3Aread%2Cusers%3Aread&amp;user_scope=&amp;redirect_uri=https%3A%2F%2Fketchup.fly.dev%2Fusers%2Fslack%2Foauth%2Fcallback&amp;client_id=780534586549.5861369223377"
           className="btn btn-neutral btn-sm normal-case text-sm"
-          onClick={handleSlackAuth}
         >
-          Connect Slack
-        </button>
+          Add to Slack
+        </a>
       );
 
       // case 2: own profile + slack -> return disconnect button
