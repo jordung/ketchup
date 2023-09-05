21ed8a8d7f7b6200ae73d8cafae90ba8cd1ca67c
jordung <jordanayd@gmail.com>
2023-09-05 11:23:48 +0800

    bugfix socket

--- frontend/src/utils/socket-client.js
+++ frontend/src/utils/socket-client.js
@@ -3,11 +3,12 @@ import { io } from "socket.io-client";
 // export const socket = io(process.env.REACT_APP_DB_API);
 export const socket = io("https://ketchup.fly.dev", {
   withCredentials: true,
-  transportOptions: {
-    polling: {
-      extraHeaders: {
-        "Access-Control-Allow-Origin": "https://theketchupcorner.netlify.app",
-      },
-    },
-  },
+  transports: ["websocket"],
+  //   transportOptions: {
+  //     polling: {
+  //       extraHeaders: {
+  //         "Access-Control-Allow-Origin": "https://theketchupcorner.netlify.app",
+  //       },
+  //     },
+  //   },
 });
