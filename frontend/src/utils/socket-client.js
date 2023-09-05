a3ea0522edf06df3de119de231722658a3de599c
jordung <jordanayd@gmail.com>
2023-09-05 13:01:04 +0800

    bugfix socket

--- frontend/src/utils/socket-client.js
+++ frontend/src/utils/socket-client.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 // export const socket = io(process.env.REACT_APP_DB_API);
 export const socket = io("https://ketchup.fly.dev", {
-  withCredentials: true,
+  //   withCredentials: true,
   transportOptions: {
     polling: {
       extraHeaders: {
